b7d4752c0bd66e1cd5f882001d30f042a5f69aba
Ahmad Tharwat <ahmad_20th@tuta.io>
2024-06-23 13:51:40 +0300

    PHP Env Test

---
+++ src/PHP-Task-01.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Syllabus</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <!-- <header>
+        <a href="https://iti.gov.eg">
+            <img id="logo" src="../imgs/ITI_logo.png" height="50" alt="iti logo">
+        </a>
+        <nav id="buttons">
+            <a href="../index.html">Home</a>
+            <a href="./syllabus.html">Road Map</a>
+            <a href="./registration_form.html">Registration Form</a>
+            <a href="./JS-Task-01.html">JS Task-01</a>
+            <a href="./JS-Task-02.html">JS Task-02</a>
+            <a href="./JS-Task-02.html">JS Task-03</a>
+            <a href="./PHP-Task-01.html" class="active">PHP Taks-01</a>
+            <a href="https://iti.gov.eg/contact-us">Contact</a>
+        </nav>
+    </header>
+    <div> -->
+    <?php
+            echo "Text";
+        ?>
+
+
+    <!-- </div>
+
+    <footer>
+        <hr>
+        <p style="text-align: center;">
+            Copyright 2024 by Noha Salah &copy;. All Rights Reserved.
+        </p>
+    </footer> -->
+</body>
+
+</html>
